f0921f6cc5ba4f84fd1d480213361d0a8b722f14
FaxHack™ <agarstarm@gmail.com>
2025-07-14 02:12:11 +0100

    Reworked it Manual Y Level
    Fixed it going down instead of up

--- src/main/java/com/stash/hunt/modules/AFKVanillaFly.java
+++ src/main/java/com/stash/hunt/modules/AFKVanillaFly.java
@@ -2,12 +2,16 @@ package com.stash.hunt.modules;
 
 import com.stash.hunt.Addon;
 import com.stash.hunt.Utils;
+
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.orbit.EventHandler;
-import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
-import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.utils.player.InvUtils;
+import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.Items;
 
 public class AFKVanillaFly extends Module {
@@ -32,7 +36,7 @@ public class AFKVanillaFly extends Module {
 
     private final Setting<Boolean> useManualY = sgGeneral.add(new BoolSetting.Builder()
         .name("Use Manual Y Level")
-        .description("Use a manually set Y level instead of the Y level where module was activated.")
+        .description("Use a manually set Y level instead of the Y level when activated.")
         .defaultValue(false)
         .build()
     );
@@ -61,48 +65,30 @@ public class AFKVanillaFly extends Module {
 
         double currentY = mc.player.getY();
 
-        if (mc.player.isFallFlying() && !useManualY.get()) {
+        if (mc.player.isFallFlying()) {
             if (yTarget == -1 || !launched) {
-                yTarget = currentY;
+                if (useManualY.get()) {
+                    yTarget = manualYLevel.get();
+                } else {
+                    yTarget = currentY;
+                }
                 launched = true;
             }
 
             // will prevent from flying straight down into the ground - adjust y range if player moves vertical
-            double yDiffFromLock = currentY - yTarget;
-            if (Math.abs(yDiffFromLock) > 10.0) {
-                yTarget = currentY; // reset the current y-level to maintain
-                info("Y-lock reset due to altitude deviation.");
+            // but only if not using manual Y level
+            if (!useManualY.get()) {
+                double yDiffFromLock = currentY - yTarget;
+                if (Math.abs(yDiffFromLock) > 10.0) {
+                    yTarget = currentY; // reset the current y-level to maintain
+                    info("Y-lock reset due to altitude deviation.");
+                }
             }
 
             double yDiff = currentY - yTarget;
 
             if (Math.abs(yDiff) > 10.0) {
-                targetPitch = (float) (-Math.atan2(yDiff, 100) * (180 / Math.PI));
-            } else if (yDiff > 2.0) {
-                targetPitch = 10f;
-            } else if (yDiff < -2.0) {
-                targetPitch = -10f;
-            } else {
-                targetPitch = 0f;
-            }
-
-            float currentPitch = mc.player.getPitch();
-            float pitchDiff = targetPitch - currentPitch;
-            mc.player.setPitch(currentPitch + pitchDiff * 0.1f);
-
-            if (System.currentTimeMillis() - lastRocketUse > fireworkDelay.get()) {
-                tryUseFirework();
-            }
-        } else if (mc.player.isFallFlying() && useManualY.get()) {
-            if (yTarget == -1 || !launched) {
-                yTarget = manualYLevel.get();
-                launched = true;
-            }
-
-            double yDiff = currentY - yTarget;
-
-            if (Math.abs(yDiff) > 10.0) {
-                targetPitch = (float) (-Math.atan2(yDiff, 100) * (180 / Math.PI));
+                targetPitch = (float) (Math.atan2(yDiff, 100) * (180 / Math.PI));
             } else if (yDiff > 2.0) {
                 targetPitch = 10f;
             } else if (yDiff < -2.0) {
@@ -129,18 +115,15 @@ public class AFKVanillaFly extends Module {
         }
     }
 
-
     public void resetYLock() {
         yTarget = -1;
         launched = false;
     }
 
-
     @EventHandler
     private void onTick(TickEvent.Pre event) {
         tickFlyLogic();
     }
-
 
     private void tryUseFirework() {
         FindItemResult hotbar = InvUtils.findInHotbar(Items.FIREWORK_ROCKET);
