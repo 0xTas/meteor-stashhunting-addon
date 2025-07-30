dfa88ebeea8792e9c8d3c7fda94ac82947fd932f
miles352 <mtdaly@ncsu.edu>
2025-07-30 11:58:53 -0400

    Fixed death message detection on DiscordNotifs

--- src/main/java/com/stash/hunt/modules/DiscordNotifs.java
+++ src/main/java/com/stash/hunt/modules/DiscordNotifs.java
@@ -94,8 +94,8 @@ public class DiscordNotifs extends Module
     );
 
     private final Setting<Boolean> chat = sgGeneral.add(new BoolSetting.Builder()
-        .name("Logs chat messages.")
-        .description("Logs all chat messages")
+        .name("Chat Messages")
+        .description("Logs chat messages")
         .defaultValue(false)
         .visible(() -> !logAll.get())
         .build()
@@ -206,7 +206,7 @@ public class DiscordNotifs extends Module
         for (Text sibling : message.getSiblings())
         {
             TextColor color = sibling.getStyle().getColor();
-            if (color != null && color.getRgb() == 11141120)
+            if (color != null && color.getRgb() == 43690)
             {
                 handleMessage(message.getString(), MessageType.DEATH);
                 return;
